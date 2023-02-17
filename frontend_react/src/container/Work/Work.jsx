24e73099415554647b443f0b2d3848fa7d2e6f0b
ChrisJRamirez <chrisjamesramirez@gmail.com>
2023-02-16 19:40:40 -0500

    adding in filter for TypeScript projects

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -43,7 +43,7 @@ const Work = () => {
       </h2>
 
       <div className="app__work-filter">
-        {["UI/UX", "Web App", "Typescript", "React JS", "All"].map(
+        {["UI/UX", "Web App", "TypeScript", "React JS", "All"].map(
           (item, index) => (
             <div
               key={index}
