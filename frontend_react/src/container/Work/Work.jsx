ada7f281b167a843a74e978428717300da97f3fc
ChrisJRamirez <chrisjamesramirez@gmail.com>
2023-03-01 17:13:44 -0500

    attempt 3 at fixing hover for phones

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -76,6 +76,10 @@ const Work = () => {
                   staggerChildren: 0.5,
                 }}
                 className="app__work-hover app__flex"
+                onClick="return true"
+                onTouchStart={() => {
+                  setAnimateCard({ y: 100, opacity: 0 });
+                }}
               >
                 <a href={work.projectLink} target="_blank" rel="noreferrer">
                   <motion.div
@@ -83,7 +87,7 @@ const Work = () => {
                     whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex"
-                    onClick=""
+                    onClick="return true"
                   >
                     <AiFillEye />
                   </motion.div>
@@ -94,10 +98,7 @@ const Work = () => {
                     whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex"
-                    onClick=""
-                    onTouchStart={() => {
-                      setAnimateCard({ y: 100, opacity: 0 });
-                    }}
+                    onClick="return true"
                   >
                     <AiFillGithub />
                   </motion.div>
